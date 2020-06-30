480ad3d2ca31376ac8eda13b8b30edf625ad8af5
Jakub Figlak <jakub.figlak@gmail.com>
2020-06-30 19:14:48 +0200

    Create a sample repo

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { plPL } from '@material-ui/core/locale';
+
+export const theme = createMuiTheme(
+    {
+        palette: {
+            primary: { main: '#1976d2' },
+        },
+    },
+    plPL
+);
